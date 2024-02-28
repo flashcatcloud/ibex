aba73ed4a725962ff11d9f8e298a81f5adc3dd20
wang <2563347014@qq.com>
2024-02-28 15:02:05 +0800

    refactor: to integrate n9e

--- src/cmd/main.go
+++ src/cmd/main.go
@@ -2,57 +2,25 @@ package main
 
 import (
 	"fmt"
-	"os"
-
 	"github.com/toolkits/pkg/net/tcpx"
 	"github.com/toolkits/pkg/runner"
 	"github.com/ulricqin/ibex/src/agentd"
 	"github.com/ulricqin/ibex/src/server"
-	"github.com/urfave/cli/v2"
 )
 
 // VERSION go build -ldflags "-X main.VERSION=x.x.x"
 var VERSION = "not specified"
 
-func main() {
-	app := cli.NewApp()
-	app.Name = "ibex"
-	app.Version = VERSION
-	app.Usage = "Ibex, running scripts on large scale machines"
-	app.Commands = []*cli.Command{
-		newServerCmd(),
-		newAgentdCmd(),
-	}
-	app.Run(os.Args)
-}
+func NewServerCmd() {
+	printEnv()
 
-func newServerCmd() *cli.Command {
-	return &cli.Command{
-		Name:  "server",
-		Usage: "Run server",
-		Flags: []cli.Flag{
-			&cli.StringFlag{
-				Name:    "conf",
-				Aliases: []string{"c"},
-				Usage:   "specify configuration file(.json,.yaml,.toml)",
-			},
-		},
-		Action: func(c *cli.Context) error {
-			printEnv()
+	tcpx.WaitHosts()
 
-			tcpx.WaitHosts()
+	var opts []server.ServerOption
+	opts = append(opts, server.SetVersion(VERSION))
+	// parse config file
 
-			var opts []server.ServerOption
-			if c.String("conf") != "" {
-				opts = append(opts, server.SetConfigFile(c.String("conf")))
-			}
-			opts = append(opts, server.SetVersion(VERSION))
-			// parse config file
-
-			server.Run(true, opts...)
-			return nil
-		},
-	}
+	server.Run(true, opts...)
 }
 
 func NewEdgeServerCmd() {
@@ -66,30 +34,12 @@ func NewEdgeServerCmd() {
 	server.Run(false, opts...)
 }
 
-func newAgentdCmd() *cli.Command {
-	return &cli.Command{
-		Name:  "agentd",
-		Usage: "Run agentd",
-		Flags: []cli.Flag{
-			&cli.StringFlag{
-				Name:    "conf",
-				Aliases: []string{"c"},
-				Usage:   "specify configuration file(.json,.yaml,.toml)",
-			},
-		},
-		Action: func(c *cli.Context) error {
-			printEnv()
+func NewAgentdCmd() {
+	printEnv()
+	var opts []agentd.AgentdOption
+	opts = append(opts, agentd.SetVersion(VERSION))
 
-			var opts []agentd.AgentdOption
-			if c.String("conf") != "" {
-				opts = append(opts, agentd.SetConfigFile(c.String("conf")))
-			}
-			opts = append(opts, agentd.SetVersion(VERSION))
-
-			agentd.Run(opts...)
-			return nil
-		},
-	}
+	agentd.Run(opts...)
 }
 
 func printEnv() {
