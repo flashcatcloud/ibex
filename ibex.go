7cf99e2d9aa78e6a91cecbb6397f35a8f2c8ab58
wang <2563347014@qq.com>
2024-02-29 17:43:58 +0800

    fix: auto get localAddr as scheduler in ibex center

--- ibex.go
+++ ibex.go
@@ -1,13 +1,16 @@
 package ibex
 
+import "C"
 import (
+	"fmt"
 	"github.com/redis/go-redis/v9"
-	"github.com/ulricqin/ibex/src/models/migrate"
 	"github.com/ulricqin/ibex/src/server/config"
 	"github.com/ulricqin/ibex/src/server/rpc"
 	"github.com/ulricqin/ibex/src/server/timer"
 	"github.com/ulricqin/ibex/src/storage"
 	"gorm.io/gorm"
+	"os"
+	"strings"
 )
 
 func EdgeServerStart(cache redis.Cmdable, rpcListen string, api config.CenterApi) {
@@ -24,9 +27,9 @@ func EdgeServerStart(cache redis.Cmdable, rpcListen string, api config.CenterApi
 
 func CenterServerStart(db *gorm.DB, cache redis.Cmdable, rpcListen string) {
 	config.C.IsCenter = true
+	config.C.Heartbeat.LocalAddr = schedulerAddrGet(rpcListen)
 
 	storage.DB = db
-	migrate.Migrate()
 	storage.Cache = cache
 
 	rpc.Start(rpcListen)
@@ -36,4 +39,16 @@ func CenterServerStart(db *gorm.DB, cache redis.Cmdable, rpcListen string) {
 	go timer.Heartbeat()
 	go timer.Schedule()
 	go timer.CleanLong()
+}
+
+func schedulerAddrGet(rpcListen string) string {
+	ip := fmt.Sprint(config.GetOutboundIP())
+	if ip == "" {
+		fmt.Println("heartbeat ip auto got is blank")
+		os.Exit(1)
+	}
+
+	port := strings.Split(rpcListen, ":")[1]
+	localAddr := ip + ":" + port
+	return localAddr
 }
