fb34e02bc7aa25d04cdd64c983d6a0774f33e532
zhaochengpro <147230368+NeganZhao@users.noreply.github.com>
2024-06-21 00:12:33 +0800

    Update method.go

--- src/server/rpc/method.go
+++ src/server/rpc/method.go
@@ -2,11 +2,10 @@ package rpc
 
 import (
 	"fmt"
+	"github.com/toolkits/pkg/logger"
 
 	"github.com/flashcatcloud/ibex/src/models"
 	"github.com/flashcatcloud/ibex/src/types"
-
-	"github.com/toolkits/pkg/logger"
 )
 
 // Ping return string 'pong', just for test
@@ -37,6 +36,7 @@ func (*Server) GetTaskMeta(id int64, resp *types.TaskMetaResponse) error {
 
 func (*Server) Report(req types.ReportRequest, resp *types.ReportResponse) error {
 	if req.ReportTasks != nil && len(req.ReportTasks) > 0 {
+		fmt.Println("task req", req)
 		err := handleDoneTask(req)
 		if err != nil {
 			resp.Message = err.Error()
@@ -63,16 +63,19 @@ func handleDoneTask(req types.ReportRequest) error {
 	count := len(req.ReportTasks)
 	for i := 0; i < count; i++ {
 		t := req.ReportTasks[i]
-		exist, isEdgeAlertTriggered := models.CheckExistAndEdgeAlertTriggered(req.Ident, t.Id)
-		// ibex agent可能会重复上报结果，如果任务已经不在task_host_doing缓存中了，说明该任务已经MarkDone了，不需要再处理
-		if !exist {
-			continue
-		}
 
-		err := models.MarkDoneStatus(t.Id, t.Clock, req.Ident, t.Status, t.Stdout, t.Stderr, isEdgeAlertTriggered)
-		if err != nil {
-			logger.Errorf("cannot mark task done, id:%d, hostname:%s, clock:%d, status:%s, err: %v", t.Id, req.Ident, t.Clock, t.Status, err)
-			return err
+		if t.Status == "success" {
+			exist, isEdgeAlertTriggered := models.CheckExistAndEdgeAlertTriggered(req.Ident, t.Id)
+			// ibex agent可能会重复上报结果，如果任务已经不在task_host_doing缓存中了，说明该任务已经MarkDone了，不需要再处理
+			if !exist {
+				continue
+			}
+
+			err := models.MarkDoneStatus(t.Id, t.Clock, req.Ident, t.Status, t.Stdout, t.Stderr, isEdgeAlertTriggered)
+			if err != nil {
+				logger.Errorf("cannot mark task done, id:%d, hostname:%s, clock:%d, status:%s, err: %v", t.Id, req.Ident, t.Clock, t.Status, err)
+				return err
+			}
 		}
 	}
 
