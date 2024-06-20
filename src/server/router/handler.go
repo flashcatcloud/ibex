198687911bb4da9c6828b55bf2dfbcc9b9aa489b
zhaochengpro <147230368+NeganZhao@users.noreply.github.com>
2024-06-20 16:03:33 +0800

    a

--- src/server/router/handler.go
+++ src/server/router/handler.go
@@ -87,6 +87,7 @@ func taskHostStdout(c *gin.Context) {
 	}
 
 	url := fmt.Sprintf("http://%s:%d/output/%d/stdout.json", host, config.C.Output.AgtdPort, id)
+	fmt.Println("ibex", url)
 	client := &http.Client{
 		CheckRedirect: func(req *http.Request, via []*http.Request) error {
 			return http.ErrUseLastResponse
